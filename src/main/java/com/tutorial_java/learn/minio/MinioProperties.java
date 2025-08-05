a7394b276aa3918b2ceb469ed2ef909097054c23
Mr Esbatu <qorri24j@gmail.com>
2025-08-05 20:13:49 +0700

    MinioProperties

---
+++ src/main/java/com/tutorial_java/learn/minio/MinioProperties.java
@@ -0,0 +1,37 @@
+package com.tutorial_java.learn.minio;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "minio")
+public class MinioProperties {
+    private String url;
+    private String accessKey;
+    private String secretKey;
+
+    // Getters and setters
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+}
