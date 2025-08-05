9e17de159477221f54b1001250f02f8b9a45441a
Mr Esbatu <qorri24j@gmail.com>
2025-08-05 20:14:16 +0700

    LearnApplicationTests

---
+++ src/test/java/com/tutorial_java/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial_java.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
